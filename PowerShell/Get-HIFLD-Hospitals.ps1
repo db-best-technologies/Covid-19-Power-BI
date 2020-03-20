988e05525f66f126bf09a89347cae5293d56a8d5
Bill Ramos <billramo@hotmail.com>
2020-03-19 18:41:10 -0700

    PowerBI Report Update for HIFLD data

--- PowerShell/Get-HIFLD-Hospitals.ps1
+++ PowerShell/Get-HIFLD-Hospitals.ps1
@@ -10,6 +10,7 @@ $OutputPathData = ".\Data-Files\HIFLDHospitals.csv"
 $OutputPathMetadata = ".\Data-Files\HIFLDHospitalsMetadata.csv"
 $HIFLDHospitalsURL = "https://opendata.arcgis.com/datasets/6ac5e325468c4cb9b905f1728d6fbf0f_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D"
 
+
 $HTML = Invoke-WebRequest -URI $HIFLDHospitalsURL
 $HTML.Content | Out-File -FilePath $OutputPathData
 $HTML.BaseResponse
